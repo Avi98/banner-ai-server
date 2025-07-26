4fa6799f6bf99dd1301755bbcef7cc709b3cf76a
Avi98 <avinash.kumar.cs92@gmail.com>
2025-07-26 23:44:42 +0530

    feat: .env change

--- config/env_variables.py
+++ config/env_variables.py
@@ -1,37 +1,70 @@
 import os
-from pydantic import ConfigDict
-from pydantic_settings import BaseSettings
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 DOT_ENV_FILE = os.path.join(os.path.dirname(__file__), ".env")
 
 
 class Settings(BaseSettings):
-    model_config = ConfigDict(extra="ignore", env_file=DOT_ENV_FILE)
+    model_config = SettingsConfigDict(
+        extra="ignore", env_file=DOT_ENV_FILE, case_sensitive=True
+    )
 
     app_name: str = "banner_ai_server"
-    hf_token: str = ""
-    openai_api_key: str
-    hf_token_inference: str = ""
-    qwen_model_endpoint: str = ""
-    stable_diffusion_endpoint: str
-    google_application_credentials: str
-    google_project_id: str
-    google_server_location: str
+    google_application_credentials: str = Field(
+        default=os.getenv("GOOGLE_APPLICATION_CREDENTIALS", ""),
+        description="Path to the Google Application Credentials JSON file",
+    )
+    google_project_id: str = Field(
+        default=os.getenv("GOOGLE_PROJECT_ID", ""),
+        description="Google Cloud Project ID",
+    )
+    google_server_location: str = Field(
+        default=os.getenv("GOOGLE_SERVER_LOCATION", ""),
+        description="Google Cloud Server Location",
+    )
 
     # aws S3
-    aws_region: str = ""
-    s3_bucket_name: str = ""
-    aws_access_key_id: str = ""
-    aws_secret_access_key: str = ""
+    aws_region: str = Field(
+        default=os.getenv("AWS_REGION", ""), description="AWS Region"
+    )
+    s3_bucket_name: str = Field(
+        default=os.getenv("S3_BUCKET_NAME", ""), description="S3 Bucket Name"
+    )
+    aws_access_key_id: str = Field(
+        default=os.getenv("AWS_ACCESS_KEY_ID", ""), description="AWS Access Key ID"
+    )
+    aws_secret_access_key: str = Field(
+        default=os.getenv("AWS_SECRET_ACCESS_KEY", ""),
+        description="AWS Secret Access Key",
+    )
 
     # DB Configuration
-    DB_HOST: str = "localhost"
-    DB_PORT: str = "5432"
-    DB_USER: str = "postgres"
-    DB_PASSWORD: str = ""
-    DB_NAME: str = "bannerDb"
-    DATABASE_URL: str = ""
+    DB_HOST: str = Field(
+        default=os.getenv("DB_HOST", "localhost"), description="Database Host"
+    )
+    DB_PORT: str = Field(
+        default=os.getenv("DB_PORT", "5432"), description="Database Port"
+    )
+    DB_USER: str = Field(
+        default=os.getenv("DB_USER", "postgres"), description="Database User"
+    )
+    DB_PASSWORD: str = Field(
+        default=os.getenv("DB_PASSWORD", "postgres"), description="Database Password"
+    )
+    DB_NAME: str = Field(
+        default=os.getenv("DB_NAME", "bannerDb"), description="Database Name"
+    )
+    DATABASE_URL: str = Field(
+        default=os.getenv("DATABASE_URL", ""), description="Database URL"
+    )
+
+    # cors
+    ALLOWED_ORIGIN: str = Field(
+        default=os.getenv("ALLOWED_ORIGIN", "http://localhost:3000"),
+        description="Origins allowed for headers",
+    )
 
     @property
     def get_database_url(self) -> str:
